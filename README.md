fdbb4d133baf9b90ef32d303f3e527194e1d44ae
shrikant9793 <shrinaths2@gmail.com>
2024-01-14 17:03:44 +0530

    requiremets added

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # End-to-end-ML-Project-Red-Wine-Project
 
-git config --global user.email "shrinaths2@gmail.com"
-git config --global user.name "shrikant9793"
+git config --global user.email "shr********@gmail.com"
+git config --global user.name "shr******"
+
+'''bash
+conda create -n red_wine_mlproject python=3.8 -y
+
+conda activate red_wine_mlproject
+
+pip install -r requirement.txt
+
+
+'''
---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open('README.md', 'r', encoding = 'utf-8') as f:
+    long_description= f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME= "End-to-end-ML-Project-Red-Wine-Project"
+AUTHOR_USER_NAME = "shrikant9793"
+SRC_REPO = "mlproject"
+AUTHOR_EMAIL = "Shrinaths2@gmail.com"
+
+
+# setuptools.setup(
+#     name= SRC_REPO,
+#     version= __version__,
+#     author = AUTHOR_USER_NAME,
+#     author_email = AUTHOR_EMAIL,
+#     description = "A small python package for ML app",
+#     long_description = long_description,
+#     long_description_content = "text/markdown",
+#     url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+#     project_urls = {
+#         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issue",
+#     },
+#     package_dir = {"": "src"},
+#     packages = setuptools.find(where= "src")
+# )
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
